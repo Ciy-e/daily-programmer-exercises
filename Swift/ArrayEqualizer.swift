07c058af6f31f225c9a46b5cb3b01c81ea1a3278
Ciy-e <kiahadjkhan@gmail.com>
2019-02-07 17:55:59 -0500

    solution to another exercise

--- Swift/ArrayEqualizer.swift
+++ Swift/ArrayEqualizer.swift
@@ -16,3 +16,22 @@ func equalizeArray(arr: [Int]) -> Int {
     return  arr.count - maxValue
 
 }
+
+/*
+* Birthday cake candles problem solution, similar algorithm so adding this here.
+*/
+func birthdayCakeCandles(ar: [Int]) -> Int {
+        var hashTable = [Int : Int]()
+    for i in ar {
+        hashTable[i] = (hashTable[i] ?? 0) + 1
+    }
+    var maxValue = 0
+    for (_ , count) in hashTable {
+        if count > maxValue {
+            maxValue = count
+        }
+    }
+    return maxValue
+
+
+}
