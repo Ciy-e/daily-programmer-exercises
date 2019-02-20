56ed04c151dc25689c77793d610d01bfa611c253
Ciy-e <kiahadjkhan@gmail.com>
2019-02-19 19:14:51 -0500

    linked list adding an element exercise

--- Python/linked_list_exercises.py
+++ Python/linked_list_exercises.py
@@ -6,3 +6,22 @@ def printLinkedList(head):
     while cur:
         print(cur.data)
         cur=cur.next
+
+# inserts a node at the given index of a singly linked list and returns a pointer to the head of the new list.
+def insertNodeAtPosition(head, data, position):
+    cur = head
+    newNode = SinglyLinkedListNode(data)
+    added = False
+    ctr = 0
+    if position == 0:
+        newNode.next = head
+        head = newNode
+        added = True
+    while not added and cur:
+        if ctr == position-1:
+            newNode.next = cur.next
+            cur.next = newNode
+            added = True
+        cur=cur.next
+        ctr+=1
+    return head
